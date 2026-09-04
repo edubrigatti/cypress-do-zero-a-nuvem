59c2623380866f08999c38bf9071f8761794a055
Eduardo Brigatti <edubrigatti74@gmail.com>
2026-09-04 15:59:52 -0300

    Break the app on purpose

--- src/script.js
+++ src/script.js
@@ -33,7 +33,7 @@ document.querySelector('button[type="submit"]').addEventListener(
       !emailField.value ||
       !textareaField.value
     ) {
-      return showAndHideErrorMessage()
+      //return showAndHideErrorMessage()
     }
     if (isPhoneRequired && !phoneField.value) {
       return showAndHideErrorMessage()
